00239a2bcd4c2bd11b61d639180e5df8cc3297ac
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-05-31 13:37:02 +0200

    Added select elements using getElementByID()

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//-----Js script -------//
+//4.2 JavaScript Requirements//
+//1. Select elements using using getElementByID() or querySelect()//
+const nameInput = document.getElementById("name"); 
+const markInput = document.getElementById("mark");
+const ResultList = document.getElementById("results");
+const studentList = document.getElementById ("list");
+const emptyMessage = document.getElementById("message");
